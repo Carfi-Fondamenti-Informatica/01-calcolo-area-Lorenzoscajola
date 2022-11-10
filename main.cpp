ef1eca94f8daa6672241052835b13643e865bd11
Lorenzoscajola <117931214+Lorenzoscajola@users.noreply.github.com>
2022-11-10 18:10:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
-using namespace std;
+using namespace std; 
 
-int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+int main () {
+	cout<<"Calcolo area triangolo, quadrato, rettangolo e trapezio; inserisci le variabili a, b e c:"<<endl;
+	float a ;
+	float b ;
+	float c ;
+	
+	cout<<"Variabile a (Base triangolo, lato rettangolo, lato quadrato):";
+	cin >> a ;
+	cout<<"Variabile b (Lato rettangolo, altezza triangolo):";
+	cin >> b ;
+	cout<<"Variabile c (Altezza trapezio):";
+	cin >> c ;
+	
+	cout<<" "<<endl;
+	cout<<" "<<endl;
+	cout<<" "<<endl; 
+	
+	cout<<"Area triangolo=";
+	cout<<(a*b)/2<<endl; 
+	cout<<"Area quadrato=";
+	cout<<(a*a)<<endl; 
+	cout<<"Area rettangolo=";
+	cout<<(a*b)<<endl; 
+	cout<<"Area trapezio=";
+	cout<<(((a+b)*c)/2)<<endl; 
+	
+	return 0; 
+	
 }
